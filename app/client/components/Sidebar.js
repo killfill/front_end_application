5237645b3956434080484f1eb1f5466c1742b0f9
Phillip Neumann <pneumann@gmail.com>
2014-07-07 13:23:15 -0400

    Browserify & watchify tasks. Run npm run watchify to continus generation of all.js!

---
+++ app/client/components/Sidebar.js
@@ -0,0 +1,9 @@
+/**
+  * @jsx React.DOM
+  */
+
+module.exports = React.createClass({
+	render: function() {
+		return <h1>!</h1>
+	}
+})
---
+++ app/client/index.js
@@ -0,0 +1,7 @@
+/**
+  * @jsx React.DOM
+  */
+
+var Sidebar = require('./components/Sidebar')
+
+React.renderComponent(<Sidebar/>, document.getElementsByClassName('sidebar')[0])
